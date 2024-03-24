1a29868b6e09a5ea48edd3af5023b3aa08882723
NatsuCamellia <natsucamellia@gmail.com>
2024-03-24 20:21:49 +0800

    Implement LRU interface

--- mp2/xv6/kernel/lru.c
+++ mp2/xv6/kernel/lru.c
@@ -9,29 +9,43 @@
 #include "proc.h"
 
 void lru_init(lru_t *lru){
-	panic("not implemented yet\n");
+	lru->size = 0;
 }
 
 int lru_push(lru_t *lru, uint64 e){
-	panic("not implemented yet\n");
+	int idx = lru_find(lru, e);
+	if (idx != -1)
+		lru_pop(lru, idx);
+	if (lru_full(lru)) 
+		lru_pop(lru, 0);
+	lru->bucket[lru->size++] = e;
 }
 
 uint64 lru_pop(lru_t *lru, int idx){
-	panic("not implemented yet\n");
+	if (idx < 0 || idx >= lru->size)
+		panic("lru_pop: idx\n");
+	uint64 e = lru->bucket[idx];
+	for (int i = idx + 1; i < lru->size; i++)
+		lru->bucket[i - 1] = lru->bucket[i];
+	lru->size--;
+	return e;
 }
 
 int lru_empty(lru_t *lru){
-	panic("not implemented yet\n");
+	return lru->size == 0;
 }
 
 int lru_full(lru_t *lru){
-	panic("not implemented yet\n");
+	return lru->size == PG_BUF_SIZE;
 }
 
 int lru_clear(lru_t *lru){
-	panic("not implemented yet\n");
+	lru->size = 0;
 }
 
 int lru_find(lru_t *lru, uint64 e){
-	panic("not implemented yet\n");
+	for (int i = 0; i < lru->size; i++)
+		if (lru->bucket[i] == e)
+			return i;
+	return -1;
 }
