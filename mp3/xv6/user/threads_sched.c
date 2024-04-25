1e40319ee17f48ff02f92efa8f2cfc0b6fb0555a
NatsuCamellia <natsucamellia@gmail.com>
2024-04-25 18:57:57 +0800

    Implement LST
    - Passed testcases of LST

--- mp3/xv6/user/threads_sched.c
+++ mp3/xv6/user/threads_sched.c
@@ -88,15 +88,68 @@ struct threads_sched_result schedule_sjf(struct threads_sched_args args)
     return r;
 }
 
+int get_slack_time(struct thread *t, int current_time) {
+    return t->current_deadline - current_time - t->remaining_time;
+}
+
+/* Returns positive integer iff `a` has higher priority than `b` */
+int lstcmp(struct thread *a, struct thread *b, int current_time) {
+    int slack_time_a = get_slack_time(a, current_time);
+    int slack_time_b = get_slack_time(b, current_time);
+    if (slack_time_a < slack_time_b)
+        return 1; 
+    else if (slack_time_a > slack_time_b)
+        return -1;
+    else
+        return b->ID - a->ID;
+}
+
 /* MP3 Part 2 - Real-Time Scheduling*/
 /* Least-Slack-Time Scheduling */
 struct threads_sched_result schedule_lst(struct threads_sched_args args)
 {
     struct threads_sched_result r;
-    // TODO: implement the least-slack-time scheduling algorithm
-    r.scheduled_thread_list_member = args.run_queue;
-    r.allocated_time = 1;
+    struct thread *thread = NULL;
+    struct thread *th = NULL;
+    list_for_each_entry(th, args.run_queue, thread_list) {
+        if (thread == NULL || lstcmp(thread, th, args.current_time) < 0)
+            thread = th;
+    }
 
+    // Queue is empty
+    if (thread == NULL) {
+        int next_release_time = __INT32_MAX__;
+        struct release_queue_entry *entry = NULL;
+        list_for_each_entry(entry, args.release_queue, thread_list) {
+            if (entry->release_time < next_release_time)
+                next_release_time = entry->release_time;
+        }
+        r.scheduled_thread_list_member = args.run_queue;
+        r.allocated_time = next_release_time - args.current_time;
+        return r;
+    }
+
+    int slack_time = get_slack_time(thread, args.current_time);
+    // printf("Slack %d\n", slack_time);
+    // Schedule it until deadline
+    if (slack_time <= 0) {
+        r.scheduled_thread_list_member = &thread->thread_list;
+        r.allocated_time = thread->current_deadline - args.current_time;
+        return r;
+    }
+
+    int time = thread->remaining_time;
+    struct release_queue_entry *entry = NULL;
+    list_for_each_entry(entry, args.release_queue, thread_list) {
+        if (entry->release_time - args.current_time >= time)
+            continue;
+        int entry_slack_time = get_slack_time(entry->thrd, entry->release_time);
+        if (entry_slack_time < slack_time || (entry_slack_time == slack_time && entry->thrd->ID < thread->ID))
+            time = entry->release_time - args.current_time;
+    }
+
+    r.scheduled_thread_list_member = &thread->thread_list;
+    r.allocated_time = time;
     return r;
 }
 
