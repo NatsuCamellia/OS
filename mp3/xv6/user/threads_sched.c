5f587043dbc5ca92ee997763a40c646c4fb33dbd
NatsuCamellia <natsucamellia@gmail.com>
2024-04-25 20:37:23 +0800

    Rewrite code to make it clean

--- mp3/xv6/user/threads_sched.c
+++ mp3/xv6/user/threads_sched.c
@@ -6,7 +6,12 @@
 
 #define NULL 0
 
-/* helper functions */
+/* ========== Helper Functions ========== */
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
 int get_next_release_time(struct threads_sched_args args) {
     int next_release_time = __INT32_MAX__;
     struct release_queue_entry *entry = NULL;
@@ -17,8 +22,35 @@ int get_next_release_time(struct threads_sched_args args) {
     return next_release_time;
 }
 
-int min(int a, int b) {
-    return a < b ? a : b;
+struct thread *get_best_ready_thread(struct threads_sched_args args, int (*cmp)(struct thread*, struct thread*, int)) {
+    struct thread *thread = NULL;
+    struct thread *th = NULL;
+    list_for_each_entry(th, args.run_queue, thread_list) {
+        if (thread == NULL || cmp(thread, th, args.current_time) < 0)
+            thread = th;
+    }
+    return thread;
+}
+
+int get_allocated_time(struct thread *thread, struct threads_sched_args args, int (*cmp)(struct thread*, struct thread*, int)) {
+    int time;
+    if (thread->is_real_time)
+        time = min(thread->remaining_time, thread->current_deadline - args.current_time);
+    else
+        time = thread->remaining_time;
+
+    struct release_queue_entry *entry = NULL;
+    list_for_each_entry(entry, args.release_queue, thread_list) {
+        int time_elapsed = entry->release_time - args.current_time;
+        if (time_elapsed >= time)
+            continue;
+        int tmp = thread->remaining_time;
+        thread->remaining_time -= time_elapsed;
+        if (cmp(thread, entry->thrd, entry->release_time) < 0)
+            time = time_elapsed;
+        thread->remaining_time = tmp;
+    }
+    return time;
 }
 
 /* default scheduling algorithm */
@@ -43,74 +75,66 @@ struct threads_sched_result schedule_default(struct threads_sched_args args)
     return r;
 }
 
-/* MP3 Part 1 - Non-Real-Time Scheduling */
-/* Weighted-Round-Robin Scheduling */
+/* ========== Weighted-Round-Rabin ========== */
+
 struct threads_sched_result schedule_wrr(struct threads_sched_args args)
 {
+    struct threads_sched_result r;
     struct thread *thread = NULL;
     struct thread *th = NULL;
     list_for_each_entry(th, args.run_queue, thread_list) {
-        // Choose the first thread in queue
         thread = th;
+        args.run_queue = args.run_queue->next;
         break;
     }
-    // Rotate the queue
-    args.run_queue = args.run_queue->next;
 
-    struct threads_sched_result r;
-    if (thread != NULL) {
-        r.scheduled_thread_list_member = &thread->thread_list;
-        int time = thread->weight * args.time_quantum;
-        r.allocated_time = (time > thread->remaining_time) ? thread->remaining_time : time;
-    } else {
+    if (thread == NULL) {
         r.scheduled_thread_list_member = args.run_queue;
-        r.allocated_time = 1;
+        r.allocated_time = get_next_release_time(args);
+        return r;
     }
 
+    r.scheduled_thread_list_member = &thread->thread_list;
+    r.allocated_time = min(thread->weight * args.time_quantum, thread->remaining_time);
     return r;
 }
 
-/* Shortest-Job-First Scheduling */
+/* ========== Shortest-Job-First ========== */
+
+int sjfcmp(struct thread *a, struct thread *b, int dummy) {
+    if (a->remaining_time < b->remaining_time)
+        return 1; 
+    else if (a->remaining_time > b->remaining_time)
+        return -1;
+    else
+        return b->ID - a->ID;
+}
+
 struct threads_sched_result schedule_sjf(struct threads_sched_args args)
 {
-    struct thread *thread = NULL;
-    struct thread *th = NULL;
-    list_for_each_entry(th, args.run_queue, thread_list) {
-        if (thread == NULL || th->remaining_time < thread->remaining_time
-            || (th->remaining_time == thread->remaining_time && th->ID < thread->ID))
-            thread = th;
-    }
-
-    int time = thread->remaining_time;
-    struct release_queue_entry *entry = NULL;
-    list_for_each_entry(entry, args.release_queue, thread_list) {
-        if (entry->release_time - args.current_time >= time)
-            continue;
-        int est_remain = thread->remaining_time - (entry->release_time - args.current_time);
-        if (entry->thrd->processing_time < est_remain ||
-            (entry->thrd->processing_time == est_remain && entry->thrd->ID < thread->ID))
-            time = entry->release_time - args.current_time;
-    }
-
     struct threads_sched_result r;
-    if (thread != NULL) {
-        r.scheduled_thread_list_member = &thread->thread_list;
-        r.allocated_time = time;
-    } else {
+    struct thread *thread = get_best_ready_thread(args, sjfcmp);
+
+    if (thread == NULL) {
         r.scheduled_thread_list_member = args.run_queue;
-        r.allocated_time = 1;
+        r.allocated_time = get_next_release_time(args);
+        return r;
     }
+
+    r.scheduled_thread_list_member = &thread->thread_list;
+    r.allocated_time = get_allocated_time(thread, args, sjfcmp);
     return r;
 }
 
-int get_slack_time(struct thread *t, int current_time) {
-    return t->current_deadline - current_time - t->remaining_time;
+/* ========== Least-Slack-Time ========== */
+
+int get_slack_time(struct thread *t, int time) {
+    return t->current_deadline - time - t->remaining_time;
 }
 
-/* Returns positive integer iff `a` has higher priority than `b` */
-int lstcmp(struct thread *a, struct thread *b, int current_time) {
-    int slack_time_a = get_slack_time(a, current_time);
-    int slack_time_b = get_slack_time(b, current_time);
+int lstcmp(struct thread *a, struct thread *b, int time) {
+    int slack_time_a = get_slack_time(a, time);
+    int slack_time_b = get_slack_time(b, time);
     if (slack_time_a < slack_time_b)
         return 1; 
     else if (slack_time_a > slack_time_b)
@@ -119,60 +143,35 @@ int lstcmp(struct thread *a, struct thread *b, int current_time) {
         return b->ID - a->ID;
 }
 
-/* MP3 Part 2 - Real-Time Scheduling*/
-/* Least-Slack-Time Scheduling */
 struct threads_sched_result schedule_lst(struct threads_sched_args args)
 {
     struct threads_sched_result r;
-    struct thread *thread = NULL;
-    struct thread *th = NULL;
-    list_for_each_entry(th, args.run_queue, thread_list) {
-        if (thread == NULL || lstcmp(thread, th, args.current_time) < 0)
-            thread = th;
-    }
+    struct thread *thread = get_best_ready_thread(args, lstcmp);
 
-    // Queue is empty
     if (thread == NULL) {
-        int next_release_time = __INT32_MAX__;
-        struct release_queue_entry *entry = NULL;
-        list_for_each_entry(entry, args.release_queue, thread_list) {
-            if (entry->release_time < next_release_time)
-                next_release_time = entry->release_time;
-        }
         r.scheduled_thread_list_member = args.run_queue;
-        r.allocated_time = next_release_time - args.current_time;
+        r.allocated_time = get_next_release_time(args) - args.current_time;
         return r;
     }
 
-    int slack_time = get_slack_time(thread, args.current_time);
-    // printf("Slack %d\n", slack_time);
-    // Schedule it until deadline
-    if (slack_time <= 0) {
+    if (get_slack_time(thread, args.current_time) <= 0) {
         r.scheduled_thread_list_member = &thread->thread_list;
         r.allocated_time = thread->current_deadline - args.current_time;
         return r;
     }
 
-    int time = thread->remaining_time;
-    struct release_queue_entry *entry = NULL;
-    list_for_each_entry(entry, args.release_queue, thread_list) {
-        if (entry->release_time - args.current_time >= time)
-            continue;
-        int entry_slack_time = get_slack_time(entry->thrd, entry->release_time);
-        if (entry_slack_time < slack_time || (entry_slack_time == slack_time && entry->thrd->ID < thread->ID))
-            time = entry->release_time - args.current_time;
-    }
-
     r.scheduled_thread_list_member = &thread->thread_list;
-    r.allocated_time = time;
+    r.allocated_time = get_allocated_time(thread, args, lstcmp);
     return r;
 }
+
+
+/* ========== Deadline-Monotonic ========== */
 
 int missedDeadline(struct thread *t, int time) {
     return t->current_deadline <= time;
 }
 
-/* Returns positive integer iff `a` has higher priority than `b` */
 int dmcmp(struct thread *a, struct thread *b, int current_time) {
     if (missedDeadline(a, current_time) && missedDeadline(b, current_time))
         return b->ID - a->ID;
@@ -188,18 +187,11 @@ int dmcmp(struct thread *a, struct thread *b, int current_time) {
     return b->ID - a->ID;
 }
 
-/* Deadline-Monotonic Scheduling */
 struct threads_sched_result schedule_dm(struct threads_sched_args args)
 {
     struct threads_sched_result r;
-    struct thread *thread = NULL;
-    struct thread *th = NULL;
-    list_for_each_entry(th, args.run_queue, thread_list) {
-        if (thread == NULL || dmcmp(thread, th, args.current_time) < 0)
-            thread = th;
-    }
+    struct thread *thread = get_best_ready_thread(args, dmcmp);
 
-    // Queue is empty
     if (thread == NULL) {
         r.scheduled_thread_list_member = args.run_queue;
         r.allocated_time = get_next_release_time(args) - args.current_time;
@@ -212,16 +204,7 @@ struct threads_sched_result schedule_dm(struct threads_sched_args args)
         return r;
     }
 
-    int time = min(thread->remaining_time, thread->current_deadline - args.current_time);
-    struct release_queue_entry *entry = NULL;
-    list_for_each_entry(entry, args.release_queue, thread_list) {
-        if (entry->release_time - args.current_time >= time)
-            continue;
-        if (dmcmp(entry->thrd, thread, entry->release_time) > 0)
-            time = entry->release_time - args.current_time;
-    }
-
     r.scheduled_thread_list_member = &thread->thread_list;
-    r.allocated_time = time;
+    r.allocated_time = get_allocated_time(thread, args, dmcmp);
     return r;
 }
